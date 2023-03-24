d3be77767f45e036dc7a42a692d4171c3aaa5150
Sauloerick <sauloe@hotmail.com>
2023-03-24 11:55:57 -0400

    Mensagem final com resposta.

--- Day4/main.js
+++ Day4/main.js
@@ -11,6 +11,6 @@ do{
     } else if(guess==num){
         alert(`PARABÉNS! Você acertou!`);
     } else {
-        alert(`Tentativas esgotadas!\nPor favor reinicie a página para tentar novamente.`);
+        alert(`Tentativas esgotadas! O número em que eu estava pensando era ${num}\nPor favor reinicie a página para tentar novamente.`);
     }
 }while (guess!=num && i!=3);
