e96c9d5b899bad2937c74be068df9a9f428e1ac4
Sauloerick <sauloe@hotmail.com>
2023-03-24 11:50:44 -0400

    Desafio do dia 4

---
+++ Day4/main.js
@@ -0,0 +1,16 @@
+var num = Math.floor(Math.random() * 10) + 1;
+var i = 0;
+
+alert(`Vamos começar!\nVocê tem 3 tentativas para tentar adivinhar um número de 1 a 10`);
+
+do{
+    i+=1;
+    var guess = prompt(`Qual número estou pensando?`);
+    if (guess!=num && i<3){
+        alert(`Errou! Tente outra vez`);
+    } else if(guess==num){
+        alert(`PARABÉNS! Você acertou!`);
+    } else {
+        alert(`Tentativas esgotadas!\nPor favor reinicie a página para tentar novamente.`);
+    }
+}while (guess!=num && i!=3);
